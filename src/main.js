73d7410eeefefb71e38daba3e2a502c23457733c
Ella <Ella@qq.com>
2019-07-08 11:19:10 +0800

    vue2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
+// 放在入口文件的模块，都会被打包
 import Vue from 'vue'
 import App from './app'
 import router from './router'
+import Header from './components/Header/Header'
+import ajax from './api/ajax'  
+import store from './vuex/store'
+import Star from './components/Star/Star.vue'
 
-/* //注册全局组件,到处都可以用
-Vue.component('Header',Header) */
+//注册全局组件,到处都可以用
+Vue.component('Header',Header)
+Vue.component('Star',Star)
 
 
 new Vue({
   el: '#app',
   render: h => h(App),
   router,
+  store,//配置store对象,才能用store
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,25 +2,45 @@ import Msite from '../pages/Msite/Msite.vue'
 import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
 export default [
     {
       path: '/',
-      redirect: '/msite'
+      redirect: '/msite',
+      meta:{
+        isShowFooter:true
+      }
     },
     {
       path: '/msite',
-      component: Msite
+      component: Msite,
+      meta:{
+        isShowFooter:true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta:{
+        isShowFooter:true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta:{
+        isShowFooter:true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta:{
+        isShowFooter:true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ] 
